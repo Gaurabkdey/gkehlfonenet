ad526a4030da093ad1d3279957e6460f4b5a9d31
Gaurabkdey <37332543+Gaurabkdey@users.noreply.github.com>
2018-05-09 15:09:45 +0530

    Update setpeer.sh

--- setpeer.sh
+++ setpeer.sh
@@ -11,10 +11,10 @@ if [[ $1 = "Org1" ]];then
 	echo "Setting to organization Org1 peer "$peerId
 	export CORE_PEER_ADDRESS=$peerId.onenet:7051
 	export CORE_PEER_LOCALMSPID=Org1MSP
-	export CORE_PEER_TLS_CERT_FILE=/opt/ws/crypto-config/peerOrganizations/onenet/peers/$peerId.onenet/tls/server.crt
-	export CORE_PEER_TLS_KEY_FILE=/opt/ws/crypto-config/peerOrganizations/onenet/peers/$peerId.onenet/tls/server.key
-	export CORE_PEER_TLS_ROOTCERT_FILE=/opt/ws/crypto-config/peerOrganizations/onenet/peers/$peerId.onenet/tls/ca.crt
-	export CORE_PEER_MSPCONFIGPATH=/opt/ws/crypto-config/peerOrganizations/onenet/users/Admin@onenet/msp
+	export CORE_PEER_TLS_CERT_FILE=/opt/network/gkehlfonenet/crypto-config/peerOrganizations/onenet/peers/peer0.onenet/tls/server.crt
+	export CORE_PEER_TLS_KEY_FILE=/opt/network/gkehlfonenet/crypto-config/peerOrganizations/onenet/peers/peer0.onenet/tls/server.key
+	export CORE_PEER_TLS_ROOTCERT_FILE=/opt/network/gkehlfonenet/crypto-config/peerOrganizations/onenet/peers/peer0.onenet/tls/ca.crt
+	export CORE_PEER_MSPCONFIGPATH=/opt/network/gkehlfonenet/crypto-config/peerOrganizations/onenet/users/Admin@onenet/msp
 fi
 
 	
